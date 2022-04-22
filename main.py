55c0195fbd1b8b0b78ef5da0ce6244a96f246d77
rakshith-ss <ghostingo@protonmail.com>
2022-04-22 08:18:46 +0530

    Added middleware for cors

--- main.py
+++ main.py
@@ -3,8 +3,24 @@ from typing import List, Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
 from datetime import datetime
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Item(BaseModel):
